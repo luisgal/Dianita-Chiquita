cd0ef6a273488155be50f61bcf12ba222244c23f
Luis Galindo <luisgalrod@gmail.com>
2021-02-10 01:00:57 -0600

    mayorDe3Numeros

---
+++ 7/mayor.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main(void){
+	int a,b,c,mayor;
+	
+	cout<<"Calcular el mayor de 3 numeros"<<endl;
+	
+	cout<<"Ingresa el valor a: ";
+	cin>>a;
+	cout<<"Ingresa el valor b: ";
+	cin>>b;
+	cout<<"Ingresa el valor c: ";
+	cin>>c;
+	
+	if(a>b && a>c){
+		mayor = a;
+	} 
+	else if(b>a && b>c){
+		mayor = b;
+	} 
+	else{
+		mayor = c;
+	}
+	
+	cout<<"EL mayor de los tres numeros es: "<<mayor<<endl;
+	
+	return 0;
+}
